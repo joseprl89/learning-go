910d18d9a4d0601e2515b5fc63058b99928dc976
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 10:00:11 +0100

    23/12/2018 10:00: Tests passed!

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 func FindMultiplesOf(x int, limit int) int {
+  var multiples []int
   for i := 1; i < limit; i++ {
     if i % x == 0 {
-      return i
+      multiples = append(multiples, i)
+      return multiples[0]
     }
   }
   return 2

421f38b16c9cb628bfc3e9c23d771877feb9347c
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 10:06:02 +0100

    23/12/2018 10:06: Tests passed!

--- main_test.go
+++ main_test.go
@@ -3,8 +3,9 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
+    expected := []int { 2 }
     total := FindMultiplesOf(2, 3)
-    if total[0] != 2 {
+    if total[0] != expected[0] {
        t.Errorf("Incorrect multiple, got: %d, want: 2.", total)
     }
 }

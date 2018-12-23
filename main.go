f9e85123d64ea717ca7d86314eddc24822b4e98e
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 10:03:07 +0100

    23/12/2018 10:03: Tests passed!

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
-func FindMultiplesOf(x int, limit int) int {
+func FindMultiplesOf(x int, limit int) []int {
   var multiples []int
   for i := 1; i < limit; i++ {
     if i % x == 0 {
       multiples = append(multiples, i)
-      return multiples[0]
+      return multiples
     }
   }
-  return 2
+  return []int { }
 }
--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
     total := FindMultiplesOf(2, 3)
-    if total != 2 {
+    if total[0] != 2 {
        t.Errorf("Incorrect multiple, got: %d, want: 2.", total)
     }
 }

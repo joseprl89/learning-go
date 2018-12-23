3d67c2a748c0ebfda3b3ba27daf0ead3bd72d890
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 09:57:46 +0100

    23/12/2018 09:57: Tests passed!

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 func FindMultiplesOf(x int, limit int) int {
-  for i := 0; i < limit; i++ {
-
+  for i := 1; i < limit; i++ {
+    if i % x == 0 {
+      return i
+    }
   }
   return 2
 }

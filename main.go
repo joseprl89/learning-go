db203e278088d0fa461a5fffe8b308a0cdb2a487
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 12:20:25 +0100

    23/12/2018 12:20: Tests passed!

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 
-func FindMultiplesOf(limit int, x ...int) []int {
-	var multiples []int
-
-	for i := 1; i < limit; i++ {
-		for j := range x {
-			var divisor = x[j]
-			if i%divisor == 0 {
-				multiples = append(multiples, i)
-				break
-			}
-		}
-	}
-
-	return multiples
-}
-
 func SumOf(array []int) int {
 	var result = 0
 	for i := 0; i < len(array); i++ {
 		result = result + array[i]
 	}
 	return result
-}
-
-func SumOfMultiples(limit int, multiples ...int) int {
-	return SumOf(FindMultiplesOf(limit, multiples...))
 }
 
 func contains(array []int, value int) bool {
---
+++ multiples.go
@@ -0,0 +1,21 @@
+package main
+
+func FindMultiplesOf(limit int, x ...int) []int {
+	var multiples []int
+
+	for i := 1; i < limit; i++ {
+		for j := range x {
+			var divisor = x[j]
+			if i%divisor == 0 {
+				multiples = append(multiples, i)
+				break
+			}
+		}
+	}
+
+	return multiples
+}
+
+func SumOfMultiples(limit int, multiples ...int) int {
+	return SumOf(FindMultiplesOf(limit, multiples...))
+}

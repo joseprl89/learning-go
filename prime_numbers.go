ac0d5a688e68e61668ef90cc7a18648338070954
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 12:50:37 +0100

    23/12/2018 12:50: Tests passed!

--- prime_numbers.go
+++ prime_numbers.go
@@ -1,7 +1,24 @@
 package main
 
+func NotDivisibleByAny(primes []int, number int) bool {
+	for i := range primes {
+		if number%primes[i] == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func PrimesUpTo(limit int) []int {
-	return []int{1, 2, 3, 5}
+	var primes []int
+
+	for i := 0; i < limit; i++ {
+		if NotDivisibleByAny(primes, i) {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
 }
 
 func HighestPrimeNumberFor(number int) int {

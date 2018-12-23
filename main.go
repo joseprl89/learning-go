18b3b803f69f9337d7a1828267bb53b4f6e89a1c
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-23 10:35:38 +0100

    23/12/2018 10:35: Tests passed!

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 func FindMultiplesOf(x int, limit int) []int {
-  var multiples []int
-  for i := 1; i <= limit; i++ {
-    if i % x == 0 {
-      multiples = append(multiples, i)
-    }
-  }
-  return multiples
+	var multiples []int
+	for i := 1; i <= limit; i++ {
+		if i%x == 0 {
+			multiples = append(multiples, i)
+		}
+	}
+	return multiples
 }
 
-func sumOf(array []int) int {
-  var result = 0
-  for i:= 0; i < len(array); i++ {
-    result = result + array[i]
-  }
-  return result
+func SumOf(array []int) int {
+	var result = 0
+	for i := 0; i < len(array); i++ {
+		result = result + array[i]
+	}
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -3,36 +3,35 @@ package main
 import "testing"
 
 func TestMultiples(t *testing.T) {
-    expected := []int { 2, 4 }
-    multiples := FindMultiplesOf(2, 4)
-    if !isEqual(multiples, expected) {
-       t.Errorf("Incorrect multiples, got: %v, want: %v.", multiples, expected)
-    }
+	expected := []int{2, 4}
+	multiples := FindMultiplesOf(2, 4)
+	if !IsEqual(multiples, expected) {
+		t.Errorf("Incorrect multiples, got: %v, want: %v.", multiples, expected)
+	}
 }
 
 func TestArraySum(t *testing.T) {
-    expected := 6
-    sum := sumOf([]int { 2, 4 })
-    if sum != expected {
-       t.Errorf("Incorrect sum of array, got: %v, want: %v.", sum, expected)
-    }
+	expected := 6
+	sum := SumOf([]int{2, 4})
+	if sum != expected {
+		t.Errorf("Incorrect sum of array, got: %v, want: %v.", sum, expected)
+	}
 }
 
+func IsEqual(lhs []int, rhs []int) bool {
+	if (lhs == nil) != (rhs == nil) {
+		return false
+	}
 
-func isEqual(lhs []int, rhs []int) bool {
-  if (lhs == nil) != (rhs == nil) {
-    return false
-  }
+	if len(lhs) != len(rhs) {
+		return false
+	}
 
-  if len(lhs) != len(rhs) {
-    return false
-  }
+	for i := range lhs {
+		if lhs[i] != rhs[i] {
+			return false
+		}
+	}
 
-  for i := range lhs {
-    if lhs[i] != rhs[i] {
-      return false
-    }
-  }
-
-  return true
+	return true
 }

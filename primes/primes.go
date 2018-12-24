9e8aecea07fcc4bed7d24a817516bffe171c0234
Josep Rodriguez <josep.rodriguez@tigerspike.com>
2018-12-24 12:30:00 +0100

    GREEN: 24/12/2018 12:30

--- primes/primes.go
+++ primes/primes.go
@@ -1,13 +1,5 @@
 package primes
 
-func NotDivisibleByAny(primes []int, number int) bool {
-	for i := range primes {
-		prime := primes[i]
-
-		if number%prime == 0 {
-			return false
-		}
-	}
-
-	return true
+func DivisibleByAny(primes []int, number int) bool {
+	return false
 }

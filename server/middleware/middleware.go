f04bf00319d5edf71fee7a395b17c233e0fe3535
Josep Rodriguez <joseprl89@gmail.com>
2018-12-26 12:01:54 +0100

    :white_check_mark: 26/12/2018 12:01

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -20,16 +20,7 @@ func (middleware *Middleware) resolveFor(request httprequest.HTTPRequest, respon
 	internalChannel := make(chan *httpresponse.HTTPResponse)
 
 	if response == nil {
-		response = &httpresponse.HTTPResponse{
-			Status:       500,
-			ReasonPhrase: "Not implemented",
-			Headers: []httpresponse.Header{
-				{
-					Name:  "Server",
-					Value: "LearningGoServer",
-				},
-			},
-		}
+		response = noResponse()
 	}
 
 	go middleware.resolveFunction(request, *response, internalChannel)
@@ -40,6 +31,19 @@ func (middleware *Middleware) resolveFor(request httprequest.HTTPRequest, respon
 		middleware.next.resolveFor(request, result, out)
 	} else {
 		out <- result
+	}
+}
+
+func noResponse() *httpresponse.HTTPResponse {
+	return &httpresponse.HTTPResponse{
+		Status:       500,
+		ReasonPhrase: "Not implemented",
+		Headers: []httpresponse.Header{
+			{
+				Name:  "Server",
+				Value: "LearningGoServer",
+			},
+		},
 	}
 }
 

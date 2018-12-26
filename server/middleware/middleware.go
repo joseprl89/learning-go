eb6206aa6486247d83c38c9838ec6fca8d5b30df
Josep Rodriguez <joseprl89@gmail.com>
2018-12-26 11:25:11 +0100

    GREEN: 26/12/2018 11:25

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -12,6 +12,6 @@ type Middleware struct {
 
 func (middleware *Middleware) ResolveFor(request httprequest.HTTPRequest, out chan httpresponse.HTTPResponse) {
 	out <- httpresponse.HTTPResponse{
-		Body: "Fail",
+		Body: "Success",
 	}
 }

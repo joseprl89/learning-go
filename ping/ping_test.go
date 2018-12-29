b252d2f26af69f13c2f6a1eeff95b72c312fd83c
Josep Rodriguez <joseprl89@gmail.com>
2018-12-29 11:13:57 +0100

    :red_circle: 29/12/2018 11:13

--- ping/ping_test.go
+++ ping/ping_test.go
@@ -37,3 +37,18 @@ func TestPingFailure(t *testing.T) {
 		t.Error("Did Ping successfully the server when failure expected.")
 	}
 }
+
+func TestPingFailureWhenErrorAndConnection(t *testing.T) {
+	channel := make(chan ping.Result)
+	sut := ping.NewWithDialer(func(host string, port int) (conn net.Conn, e error) {
+		return &mocks.Connection{}, errors.New("Fail")
+	})
+
+	go sut.Ping("google.com", 443, channel)
+
+	result := <-channel
+
+	if result.Success {
+		t.Error("Did Ping successfully the server when failure expected.")
+	}
+}

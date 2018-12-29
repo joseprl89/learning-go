c4e888feded2d3ac617d340726cfc7d0f7206b33
Josep Rodriguez <joseprl89@gmail.com>
2018-12-29 11:10:34 +0100

    :white_check_mark: 29/12/2018 11:10

--- ping/ping_test.go
+++ ping/ping_test.go
@@ -1,6 +1,7 @@
 package ping_test
 
 import (
+	"euler/mocks"
 	"euler/ping"
 	"net"
 	"testing"
@@ -9,7 +10,7 @@ import (
 func TestPing(t *testing.T) {
 	channel := make(chan ping.Result)
 	sut := ping.NewWithDialer(func(host string, port int) (conn net.Conn, e error) {
-		panic("Dont call yet")
+		return &mocks.Connection{}, nil
 	})
 
 	go sut.Ping("google.com", 443, channel)
